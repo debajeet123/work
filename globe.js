4a2e5ccf27d06414a98497263cf1615dc295748f
debajeet123 <debajeet123@gmail.com>
2025-04-20 19:32:03 -0500

    1

--- globe.js
+++ globe.js
@@ -1,6 +1,6 @@
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(45, 1, 0.1, 1000);
-camera.position.z = 12;
+camera.position.z = 7;
 
 const renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
 renderer.setSize(250, 250);
@@ -10,7 +10,7 @@ document.getElementById("cornerGlobe").appendChild(renderer.domElement);
 
 const loader = new THREE.TextureLoader();
 loader.load("https://unpkg.com/three-globe/example/img/earth-night.jpg", function (texture) {
-  const geometry = new THREE.SphereGeometry(3, 64, 64); // smaller globe
+  const geometry = new THREE.SphereGeometry(5, 64, 64); // smaller globe
   const material = new THREE.MeshPhongMaterial({ map: texture });
   const globe = new THREE.Mesh(geometry, material);
   scene.add(globe);
