8fcb32ce5bdf83f856c6714e770bee821f81d817
debajeet123 <debajeet123@gmail.com>
2025-04-24 09:31:37 -0500

    1

--- globe.js
+++ globe.js
@@ -1,34 +1,45 @@
-
 document.addEventListener("DOMContentLoaded", () => {
+  // Scene & Camera
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(45, 1, 0.1, 1000);
   camera.position.z = 3;
 
+  // Renderer
   const container = document.getElementById("cornerGlobe");
   const renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
   renderer.setSize(container.clientWidth, container.clientHeight);
   renderer.setPixelRatio(window.devicePixelRatio);
   container.appendChild(renderer.domElement);
 
+  // Lights
   scene.add(new THREE.AmbientLight(0x888888));
   const light = new THREE.DirectionalLight(0xffffff, 1);
   light.position.set(5, 3, 5);
   scene.add(light);
 
-  const loader = new THREE.TextureLoader();
-  loader.load("https://raw.githubusercontent.com/ajayns/earth-js/master/images/earth-night.jpg", texture => {
-    const geo = new THREE.SphereGeometry(1, 64, 64);
-    const mat = new THREE.MeshPhongMaterial({ map: texture, emissive: 0x111111, emissiveIntensity: 0.4 });
-    const globe = new THREE.Mesh(geo, mat);
-    scene.add(globe);
-    function animate() {
-      requestAnimationFrame(animate);
-      globe.rotation.y += 0.001;
-      renderer.render(scene, camera);
-    }
-    animate();
-  });
+  // Load dark Earth texture
+  new THREE.TextureLoader().load(
+    "https://raw.githubusercontent.com/ajayns/earth-js/master/images/earth-night.jpg",
+    texture => {
+      const geo = new THREE.SphereGeometry(1, 64, 64);
+      const mat = new THREE.MeshPhongMaterial({
+        map: texture,
+        emissive: 0x111111,
+        emissiveIntensity: 0.4
+      });
+      const globe = new THREE.Mesh(geo, mat);
+      scene.add(globe);
 
+      // Animate
+      (function animate() {
+        requestAnimationFrame(animate);
+        globe.rotation.y += 0.001;
+        renderer.render(scene, camera);
+      })();
+    }
+  );
+
+  // Responsive
   window.addEventListener("resize", () => {
     const w = container.clientWidth, h = container.clientHeight;
     renderer.setSize(w, h);
