63be64f5a9e0167073de5cd7fe05a3bf81c0b2f4
debajeet123 <debajeet123@gmail.com>
2025-04-23 19:21:31 -0500

    Update globe.js

--- globe.js
+++ globe.js
@@ -1,10 +1,4 @@
 
-// globe.js
-
-// 1. Setup Scene & Camera
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(45, 1, 0.1, 1000);
-camera.position.z = 4;
 
 document.addEventListener("DOMContentLoaded", () => {
   // 1. Scene and camera
